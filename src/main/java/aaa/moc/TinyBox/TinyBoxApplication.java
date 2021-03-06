e9548f69ca4d7b8db65b7e9af5418ef7ff591174
Martin <martinordonocaro@gmail.com>
2021-03-06 16:55:37 +0100

    initial commit

---
+++ src/main/java/aaa/moc/TinyBox/TinyBoxApplication.java
@@ -0,0 +1,14 @@
+package aaa.moc.TinyBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aaa/moc/TinyBox/TinyBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package aaa.moc.TinyBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
