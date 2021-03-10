958fd3ae2cc14b32fd0ca61d79102dc0715d45c2
Adamek4 <60627048+Adamek4@users.noreply.github.com>
2021-03-10 21:26:00 +0100

    Corregido la herencia jaja

--- src/main/java/aaa/moc/TinyBox/domain/Actividad.java
+++ src/main/java/aaa/moc/TinyBox/domain/Actividad.java
@@ -2,13 +2,11 @@ package aaa.moc.TinyBox.domain;
 
 import lombok.*;
 import org.hibernate.validator.constraints.Range;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
-
 
 
 @Entity
@@ -29,30 +27,30 @@ public class Actividad implements Serializable {
     @NotNull(message = "El campo series no puede estar vacío")
     @Range(max= 50, message = "El campo series no puede tener más de 50 series")
     @Column(length = 50)
-    protected int series;
+    private Integer series;
 
     @NotNull(message = "El campo repeticiones no puede estar vacío.")
     @Range(max= 80, message = "El campo repeticiones no puede tener más de 80 repeticiones")
     @Column(length = 80)
-    protected int repeticiones;
+    private Integer repeticiones;
 
     @NotNull(message = "El campo descanso no puede estar vacío.")
     @Range(max= 300, message = "El campo descanso no puede tener más de 300 segundos") //No creo que vaya a ver un descanso de mas de 5 min
     @Column(length = 300)
-    protected int descanso;
+    private Integer descanso;
 
     @NotNull(message = "El campo carga no puede estar vacío.")
     @Range(max=150, message = "El campo carga no puede tener más de 150 kilogramos") //Creo que adolfo no tiene tanto peso en el TinyBox
     @Column(length = 150)
-    protected int carga;
+    private Integer carga;
 
     @Size(max = 50, message = "El campo observaciones no puede tener más de 50 caracteres")
     @Column(length = 50)
-    protected String observaciones;
+    private String observaciones;
 
     @NotEmpty(message = "El campo tipo no puede estar vacío.")
     @Size(max = 50, message = "El campo foto no puede tener más de 50 caracteres")
     @Column(length = 50)
-    protected String tipo;
+    private String tipo;
 
 }
